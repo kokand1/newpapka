2a7d2d13c08a9591a9b446d04f8303b91829597e
azizaka <ziz.uz3616524@gmail.com>
2023-06-22 16:14:40 +0500

    git branch uzgardi

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -40,7 +40,6 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   // git branch qushildi
- 
 
   final String title;
 
@@ -54,24 +53,20 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       // git hub uzgartirish
-      
+      // git branch uzgartirish
+
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
       ),
       body: Center(
-        
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -88,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
